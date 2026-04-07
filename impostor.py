4526aa1ce1e248bb7d885b772c59e7ba8573445d
DawidS25 <dawid.solsnia2906@op.pl>
2026-04-07 14:06:10 +0200

    test

--- impostor.py
+++ impostor.py
@@ -1,5 +1,16 @@
+import requests
 import streamlit as st
 
 token = st.secrets["GIT_TOKEN"]
 
-st.write("Token wczytany:", token[:5] + "...")
+headers = {
+    "Authorization": f"token {token}"
+}
+
+response = requests.get("https://api.github.com/user", headers=headers)
+
+if response.status_code == 200:
+    st.success("Token działa ✅")
+    st.write(response.json()["login"])
+else:
+    st.error(f"Błąd: {response.status_code}")
